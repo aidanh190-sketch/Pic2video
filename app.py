517b096837dfe323136d340d9c598b9dff67f7f3
aidanh190-sketch <aidanh190@gmail.com>
2025-10-10 20:39:14 +0100

    Update app.py

--- app.py
+++ app.py
@@ -49,4 +49,6 @@ def render_vid():
     return send_file(out, as_attachment=True, download_name="your_video.mp4")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
